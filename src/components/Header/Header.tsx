c27081604a152b8f963f3ad844ac0e6812bb9615
Beast51 <7007500@gmail.com>
2022-05-25 12:36:01 +0300

    added link to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,12 @@ export const Header: FC<HeaderPropsType> = ({ currencys, currencySymbols }) => {
   return (
     <header className={s.header}>
       <div className="wrapper">
-        <a className={s.logo} rel="noopener noreferrer" href="/" target="">
+        <a
+          className={s.logo}
+          rel="noopener noreferrer"
+          href="http://beast51.github.io/currency-calculator"
+          target=""
+        >
           <span></span>
         </a>
         <div className={s.container}>
